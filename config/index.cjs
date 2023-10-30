2ef5e9111caf564fac02fee39f81c739ff6b5a4d
zhong2004de <115056432+zhong2004de@users.noreply.github.com>
2023-10-30 09:16:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -112,7 +112,7 @@ USE_PASSAGE: 'push-deer',
       name: '老婆0',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: '',
+      id: 'PDU21346TEyB01bFaqjkO8GRXw9I4VbNfiXW3Vjxf',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '',
